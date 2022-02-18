e40ea61db87917d4fd057d30151870f0f1f5d790
mdcarron <mdcarron.dev@gmail.com>
2022-02-18 15:47:04 -0300

    Finaliza leccion 80

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,22 +1,19 @@
-import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import { getGifs } from "../helpers/getGifs";
+import { useFetchGif } from "../hooks/useFetchGif";
 import GridItem from "./GridItem";
 
 const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs(category).then(setImages);
-  }, []);
+  const { data, loading } = useFetchGif(category);
 
   return (
     <>
       <h3>{category}</h3>
       <div className="gif-grid">
-        {images.map((i) => (
-          <GridItem {...i} key={i.id} />
-        ))}
+        {loading
+          ? "buscando gifs"
+          : data.map((d) => <GridItem {...d} key={d.id} />)}
       </div>
     </>
   );
--- src/hooks/useFetchGif.js
+++ src/hooks/useFetchGif.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 
-export const useFetchGif = () => {
+export const useFetchGif = (category) => {
   const [state, setState] = useState({
     data: [],
     loading: true,
   });
+
+  useEffect(() => {
+    getGifs(category).then((img) => {
+      setState({ data: img, loading: false });
+    });
+  }, [category]);
 
   return state;
 };
