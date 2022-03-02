119aa1cc63ca57254fe9e856c422ff8a2baa2205
mdcarron <mdcarron.dev@gmail.com>
2022-03-02 11:02:34 -0300

    Finaliza leccion 101

--- src/tests/hooks/useFetchGif.test.js
+++ src/tests/hooks/useFetchGif.test.js
@@ -2,11 +2,25 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGif } from "../../hooks/useFetchGif";
 
 describe("test on useFetchGif", () => {
-  test("should return initial value", () => {
-    const { result } = renderHook(() => useFetchGif("jujutsu kaisen"));
+  test("should return initial value", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGif("jujutsu kaisen")
+    );
     const { data, loading } = result.current;
+    await waitForNextUpdate();
 
     expect(data).toEqual([]);
     expect(loading).toBeTruthy();
+  });
+
+  test("should return initial value", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGif("jujutsu kaisen")
+    );
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+
+    expect(data.length).toBe(15);
+    expect(loading).toBeFalsy();
   });
 });
